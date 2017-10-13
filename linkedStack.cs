69da3b50e7575dacceb91fe03e9dbcb855864ac3
narekhovhannisyan <narekgilmour@gmail.com>
2017-10-13 23:25:06 +0400

    linkedStack: implemented linked stack instead of array based stack
     - added class lStack with Node class and push, pop, lastItem, isEmpty methods
     - added tests in Main function
     - push function creates new Node and inserts new value into it changing reference to previous
     - pop function goes to previous Node
     - lastItem function returns top of the stack in case of existion
     - isEmpty function returns ture if stack is empty, false in other case

---
+++ linkedStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace stack_namespace
+{
+  class linkedStack
+  {
+    static void Main(string[] args)
+    {
+      lStack stack = new lStack();
+      stack.push(5);
+      stack.push(6);
+      stack.pop();
+      stack.pop();
+      Console.WriteLine(stack.lastItem());
+      Console.WriteLine(stack.isEmpty());
+    }
+  }
+  class lStack
+  {
+    class Node
+    {
+      public int Value;
+      public Node Next;
+    }
+    Node top = null;
+
+    public void push(int value)
+    {
+      Node newNode = new Node();
+      newNode.Next = top;
+      newNode.Value = value;
+      top = newNode;
+    }
+
+    public void pop()
+    {
+      if (top.Next != null) top = top.Next;
+      else top = null;
+    }
+
+    public int? lastItem()
+    {
+      if (top != null) return top.Value;
+      else return null;
+    }
+
+    public bool isEmpty() => top == null ? true : false;
+  }
+}
